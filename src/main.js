75bfcebb12dc4cc596c1235a42d51ad638bfdac0
xRetry <marcoschwarz.at@gmail.com>
2024-03-10 12:07:49 +0100

    Simplify state

--- src/main.js
+++ src/main.js
@@ -1,148 +1,161 @@
+/** @type {[number, number]} */
 var gOffset = [0, 0];
-var gDrone;
-var gBeam;
-var gPosTarget = [0,0];
+/** @type {State} */
+var gState;
 /** @type {Sensor} */
 var gSensor;
 
-class Beam {
-    angle = 0;
-    html;
+/** 
+ * @typedef {Object} TargetState
+ * @property {[number, number]} pos
+ * @property {HTMLElement} html
+ */
+
+/** 
+ * @typedef {Object} BeamState
+ * @property {number} angle
+ * @property {HTMLElement} html
+ */
+
+/** 
+ * @typedef {Object} SensorState
+ * @property {[number, number]} pos
+ * @property {[number, number]} vel
+ * @property {HTMLElement} html
+ */
+
+
+class State {
+    /** @type {SensorState} */
+    sensor;
+    /** @type {BeamState} */
+    beam;
+    /** @type {TargetState} */
+    target;
+    /** @type {number} */
+    time;
+    /** @type {number} */
+    dt = 0;
 
     constructor() {
-        /** @type {HTMLElement} */
-        this.html = document.querySelector('.beam');
+        const htmlSensor = document.querySelector('#sensor');
+        this.sensor = {
+            html: htmlSensor,
+            pos: [htmlSensor.offsetLeft, htmlSensor.offsetTop],
+            vel: [0, 0],
+        };
+
+        this.beam = {
+            html: document.querySelector('.beam'),
+            angle: 0,
+        };
+
+        const htmlTarget = document.querySelector('.target');
+        const rect = htmlTarget.getBoundingClientRect();
+        this.target = {
+            pos: [rect.left, rect.top],
+            html: htmlTarget,
+        };
+
+        this.time = window.performance.now();
+
+        this.sensor.html.addEventListener('mousedown', handleMouseDown);
     }
 
-    rotate(state, angleRad) {
+    /**
+     * @param {[number, number]} pos
+     * @returns {void}
+     */
+    update(pos) {
+        const time = window.performance.now();
+        this.dt = time - this.time;
+        this.time = time;
+
+        this.updateSensor(pos);
+        this.updateBeam(pos);
+    }
+
+    /**
+     * @param {[number, number]} pos
+     * @returns {void}
+     */
+    updateSensor(pos) {
+        this.sensor.html.style.left = pos[0] + 'px';
+        this.sensor.html.style.top  = pos[1] + 'px';
+
+        const dx = [
+            this.sensor.html.offsetLeft - this.sensor.pos[0],
+            this.sensor.html.offsetTop - this.sensor.pos[1],
+        ];
+
+        this.sensor.pos = pos;
+        this.sensor.vel = [dx[0]/this.dt, dx[1]/this.dt];
+    }
+
+    /**
+     * @param {[number, number]} pos
+     * @param {angle} angleRad
+     * @returns {void}
+     */
+    updateBeam(pos, angleRad) {
         const angleDeg = angleRad*(180/Math.PI);
         const corrY = 500 * Math.sin(angleRad);
         const corrX = 500 - 500 * Math.cos(angleRad);
-        this.html.style.transform  = 'rotate(' + angleDeg + 'deg)';
-        this.html.style.left = (state.pos[0] + 15 - corrX) + 'px';
-        this.html.style.top  = (state.pos[1] + 15 + corrY) + 'px';
-    }
-}
-
-class Target {
-    pos = [0, 0];
-    html;
-
-    constructor() {
-        /** @type {HTMLElement} */
-        this.html = document.querySelector('.target');
-        let rect = this.html.getBoundingClientRect();
-        this.pos = [rect.left, rect.top];
-    }
-}
-
-class State {
-    pos = [0, 0];
-    vel = [0, 0];
-    time = 0;
-    beam;
-    target;
-    html;
-
-    constructor() {
-        /** @type {HTMLElement} */
-        this.html = document.querySelector('#sensor');
-
-        /** @type {Beam} */
-        this.beam = new Beam();
-
-        /** @type {Target} */
-        this.target = new Target();
-
-        this.time = window.performance.now();
-        this.pos = [this.html.offsetLeft, this.html.offsetTop];
-
-        this.html.addEventListener('mousedown', handleMouseDown);
-    }
-
-    update(pos) {
-        this.html.style.left = pos[0] + 'px';
-        this.html.style.top  = pos[1] + 'px';
-
-        const dx = [
-            this.html.offsetLeft - this.pos[0],
-            this.html.offsetTop - this.pos[1],
-        ];
-        const time = window.performance.now();
-        const dt = time - this.time;
-        this.time = time;
-
-        this.pos = pos;
-        this.vel = [dx[0]/dt, dx[1]/dt];
+        this.beam.html.style.transform  = 'rotate(' + angleDeg + 'deg)';
+        this.beam.html.style.left = (pos[0] + 15 - corrX) + 'px';
+        this.beam.html.style.top  = (pos[1] + 15 + corrY) + 'px';
     }
 
     /**
-     * @return {array<number>}
+     * @returns {[number, number]}
      */
     targetDistance() {
         return [
-            this.target.pos[0] - this.pos[0],
-            this.target.pos[1] - this.pos[1],
+            this.target.pos[0] - this.sensor.pos[0],
+            this.target.pos[1] - this.sensor.pos[1],
         ];
     }
 
-    rotateBeam(angleRad) {
-        this.beam.rotate(this, angleRad);
-    }
-
-}
-
-class SensorObservation {
-    pos = [0, 0];
-    vel = [0, 0];
-
     /**
-     * @param {SensorState} state
+     * @param {angle} angleRad
+     * @returns {void}
      */
-    constructor(state) {
-        this.pos = state.pos;
-    }
-
-    update(state) {
-        const distance = state.targetDistance();
-
-        const angle = Math.atan(distance[1]/distance[0]);
-
-        state.rotateBeam(angle);
+    rotateBeam(angleRad) {
+        this.updateBeam(this.sensor.pos, angleRad);
     }
 }
 
 class Sensor {
+    pos = [0, 0];
+    vel = [0, 0];
+
     constructor() {
-        /** @type {State} */
-        this.state = new State();
-        /** @type {SensorObservation} */
-        this.observation = new SensorObservation(this.state);
+        this.pos = gState.sensor.pos;
     }
 
-    update(pos) {
-        this.state.update(pos);
-        this.observation.update(this.state);
+    update() {
+        const distance = gState.targetDistance();
+        const angle = Math.atan(distance[1]/distance[0]);
+
+        gState.rotateBeam(angle);
     }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    gState = new State();
     gSensor = new Sensor();
+
 })
 
 function handleMouseDown(e) {
-    const pos = [
-        gSensor.state.pos - e.clientX,
-        gSensor.state.pos - e.clientY
-    ];
-    console.log(gSensor.state.pos);
-    gSensor.update(pos);
+    gState.update(gState.sensor.pos);
 
     gOffset = [
-        gSensor.state.pos - e.clientX,
-        gSensor.state.pos - e.clientY
+        gState.sensor.pos[0] - e.clientX,
+        gState.sensor.pos[1] - e.clientY
     ];
 
+    gSensor.update();
 
     addEventListener("mousemove", handleMouseMove);
     addEventListener("mouseup", handleMouseUp);
@@ -153,14 +166,11 @@ function handleMouseUp() {
 }
 
 function handleMouseMove(e) {
-    //gOffset = [0, 0];
-    //console.log(gOffset);
-
     const pos = [
         e.clientX + gOffset[0],
         e.clientY + gOffset[1],
     ];
-    gSensor.update(pos);
-
+    gState.update(pos);
+    gSensor.update();
 }
 
